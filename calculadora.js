6405d4c59ce00b27b9ff20774aa849d162ffea93
Depoa <marcosdepoa@yahoo.com.br>
2022-07-14 19:41:57 -0300

    estilizando a calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,38 @@
-var altura = document.getElementById("largura1").value;
+var areaParede1 = calcularAreaParede1();
+var largura1 = document.getElementById("largura1").value;
+var altura1 = document.getElementById("altura1").value;
 
-areaTotal = altura * largura;
+areaParede1 = largura1 + altura1;
+console.log(areaParede1)
 
-superficie = areaTotal - areaAberturas;
+function calcularAreaParede1(altura1, largura1){
+        var largura1 = Number(document.querySelector("#largura1").value);
+        var altura1 = Number(document.querySelector("#altura1").value);
+        return (largura1 * altura1).toFixed(2);
+    }
 
-tintaParede = superficie/5;
 
-variavel = tintaParede1 + tintaParede2
 
-document.getElementById("tinta-total").innerHTML = variavel
+const campoQtdeTinta = document.querySelector("#tinta-total");
 
-totalTinta = 0.0 L
-lataG = 0
-lataM = 0
-lataP = 0
-lataPP = 0
+const botao = document.querySelector("#Calcular");
 
-function distribuirLatas(totalTintas){
+botao.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    areaParede1 = largura1.value + altura1.value;
+console.log(areaParede1)
+    
+})
+
+/*
+var areaTotal = areaParede1 + areaParede2 + areaParede3 + areaParede4 - areaPortas - areaJanelas;
+
+var tintaNecessaria = areaTotal/5;
+
+
+function distribuirLatas(tintaNecessaria){
+    var totalTintas = tintaNecessaria;
     while(totalTintas > 0){
         if(totalTintas >= 18){
             lataG++;
@@ -37,8 +53,48 @@ function distribuirLatas(totalTintas){
     }
 }
 
+const campoQtdeTinta = document.querySelector("#tinta-total");
+
+
+function imprimirResultados(){
+    campoQtdeTinta.innerHTML =  
+}
+
+const botao = document.querySelector("#Calcular");
+
+botao.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+})
+*/
+
+/*var largura=parseFloat(document.getElementById("largura1").value);
+document.getElementById("largInput").innerHTML="LARGURA da parede: " + comprimento + "m";
+
+var altura=parseFloat(document.getElementById("altura1").value);
+document.getElementById("altInput").innerHTML="ALTURA da parede: " + altura + "m";
+
+var m2ParedeP1 = parseFloat((largura * altura).toFixed(2));
+document.getElementById("m2paredeResultado").innerHTML = "m² da parede: " + m2ParedeP1 + "m²";
+
+areaTotal = altura1 * largura1;
+
+superficie = areaTotal - areaAberturas;
+
+tintaParede = superficie/5;
+
+variavel = tintaParede1 + tintaParede2
+
+document.getElementById("tinta-total").innerHTML = variavel
+
+totalTinta = 0.0 L
+lataG = 0
+lataM = 0
+lataP = 0
+lataPP = 0
+
+
 distribuirLatas;
 
 if (lataG > 0) {
-    document.getElementById("tinta-total").innerHTML = lataG;
-}
+    document.getElementById("tinta-total").innerHTML = lataG;*/
